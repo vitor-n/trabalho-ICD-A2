36094df06890927e830220614e27eac01bfeea1a
Vitor <vitor.mnw@gmail.com>
2023-06-16 16:47:16 -0300

    add data loader

--- import_data.py
+++
@@ -1,6 +0,0 @@
-from bokeh.models import ColumnDataSource
-import pandas as pd 
-
-def import_df_for_bokeh(path):
-    df = pd.read_csv(path)
-    return df
---
+++ main.py
@@ -0,0 +1,3 @@
+from bokeh_data_loader import cds_from_csv
+
+sp_housing = cds_from_csv("sp_housing.csv")
