180125248231e1d16b41e669c23fc2392b6c4045
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-06-21 20:35:36 -0300

    created docstring for function

--- graph_style.py
+++ graph_style.py
@@ -1,6 +1,16 @@
 
 def apply_default_style(p):
+    """
+    
+    Receives a Bokeh figure() object and applies style elements
 
+        Parameter: Bokeh figure() object
+
+        Return: figure() object with elements of style
+        
+    """
+    
+    #Axis
     p.xaxis.axis_label_text_font_size = '15px'
     p.yaxis.axis_label_text_font_size = "15px"
     p.xaxis.axis_label_text_font_style = "bold"
