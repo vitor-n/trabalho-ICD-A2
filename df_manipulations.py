1da5696846102f516614fea63cde61648e0c6dc6
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-06-22 09:45:12 -0300

    added functions for the 1st and 3rd graphs

--- df_manipulations.py
+++ df_manipulations.py
@@ -1,7 +1,57 @@
 import pandas as pd
 from import_data import import_df_for_bokeh
 
+def df_aroma_aftertaste(datapath):
+    """
+    Creates the disctionary used in the aroma x aftertaste plot
+
+    Entries: {country : [mean aroma, mean aftertaste]}
+    """
+
+    #Fixing bad names in "Country of Origin" column
+    new_df = pd.DataFrame(datapath)
+    new_df["Country of Origin"] = new_df["Country of Origin"].replace(["Tanzania, United Republic Of"], "Tanzania")
+    new_df["Country of Origin"] = new_df["Country of Origin"].replace(["United States (Hawaii)"], "Hawaii")
+    
+    #Counts how many times each country occurs and puts that in a dictionary
+    countries_and_amount_occurences = new_df["Country of Origin"].value_counts().to_frame().reset_index()
+    countries_and_amount_occurences.columns = ["Countries", "Amount of Occurences"]
+    dict_countries_and_amount_occurences = dict(zip(countries_and_amount_occurences["Countries"],
+                                                    countries_and_amount_occurences["Amount of Occurences"]))
+
+    #Creates a dicionary of which the keys are different countries and
+    #the values are lists of the total aroma and aftertaste
+    aroma_aftertaste = {}
+    for index_of_table in range(len(datapath["Country of Origin"])):
+
+        country = datapath["Country of Origin"][index_of_table]
+        aroma = datapath["Aroma"][index_of_table]
+        aftertaste = datapath["Aftertaste"][index_of_table]
+
+        #If the country already exists in aroma_aftertaste, its value will be the current value of aroma and aftertaste.
+        #Otherwise, we add the existing value in aroma_aftertaste with the current values
+        if country not in aroma_aftertaste:
+            aroma_aftertaste[country] = [aroma, aftertaste]
+        else:
+            aroma_aftertaste[country] = [ x + y for x, y in zip(aroma_aftertaste[country], [aroma, aftertaste]) ]
+
+    #Divide the associated with each country by its amount of occurences,
+    #giving us a dictionary with the mean values
+    mean_aroma_aftertaste = aroma_aftertaste
+    for country1 in dict_countries_and_amount_occurences:
+        for country2 in aroma_aftertaste:
+            if country1 == country2:
+                mean_aroma_aftertaste[country1] = [x / dict_countries_and_amount_occurences[country1] for x in aroma_aftertaste[country1]]
+    
+    return mean_aroma_aftertaste
+
 def df_variety_method(datapath):
+    """
+    Creates the dataframe used in the variety x method plot
+
+    Columns: "Variety", "Processing Method"
+    """
+    
     #Creating new dataframe for manipulation
     df_varieties_method = datapath
 
@@ -74,6 +124,56 @@ def df_variety_method(datapath):
     new_df = {
         "Variety" : smooth_new_varieties,
         "Processing Method" : smooth_new_methods
+    }
+
+    return pd.DataFrame(new_df)
+
+def df_country_kilos(datapath):
+    """
+    Creates the dataframe used in the country x kilos plot
+
+    Columns: "Countries", "Kilos of Coffee"
+    """
+    
+    #Creating dataframe for manipulation
+    df_country_total_kilos = datapath
+
+    #Fixing bad names in "Country of Origin" column
+    df_country_total_kilos["Country of Origin"] = df_country_total_kilos["Country of Origin"].replace(["Tanzania, United Republic Of"], "Tanzania")
+    df_country_total_kilos["Country of Origin"] = df_country_total_kilos["Country of Origin"].replace(["United States (Hawaii)"], "Hawaii")
+    
+    #Deleting missing values
+    df_country_total_kilos = df_country_total_kilos.dropna(subset="Number of Bags")
+    df_country_total_kilos = df_country_total_kilos.dropna(subset="Bag Weight")
+
+    #Taking only the numerical part of "Bag Weight"'s registers
+    for index in range(len(df_country_total_kilos["Bag Weight"])):
+        entry = df_country_total_kilos["Bag Weight"].iloc[index].replace(",", ".")
+        entry = entry.split(" ")
+        df_country_total_kilos["Bag Weight"].iloc[index] = float(entry[0])
+   
+    list_of_kilos = [] #list that will contain the kilos of each lot, associated to country
+    for index in range(len(df_country_total_kilos["Number of Bags"])):
+        kilos = int(df_country_total_kilos["Number of Bags"].iloc[index])*int(df_country_total_kilos["Bag Weight"].iloc[index])
+        list_of_kilos.append(kilos)
+    
+    list_of_all_countries = [country for country in df_country_total_kilos["Country of Origin"]]
+    countries = [] #list that will contain countries without repetition
+    kilos_per_country = [] #list that will contain the kilos of each country in order
+    for index in range(len(list_of_kilos)):
+        if list_of_all_countries[index] not in countries:
+            countries.append(list_of_all_countries[index])
+            kilos_per_country.append(list_of_kilos[index])
+        else:
+            for subindex in range(len(countries)):
+                if countries[subindex] == list_of_all_countries[index]:
+                    kilos_per_country[subindex] += list_of_kilos[index]
+    
+    print(kilos_per_country)
+
+    new_df = {
+        "Countries" : countries,
+        "Kilos of Coffee" : kilos_per_country
     }
 
     return pd.DataFrame(new_df)
