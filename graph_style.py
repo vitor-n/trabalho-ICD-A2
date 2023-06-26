b00bd98779198fec20ee726db67377de7c55fc01
pedrotokar <pedrotokar2004@gmail.com>
2023-06-26 17:49:38 -0300

    Add docstrings and comments to style functions

--- graph_style.py
+++ graph_style.py
@@ -38,35 +38,51 @@ def apply_default_style(p):
     return p
 
 def apply_dotplot_style(p):
+    """
+
+    Receives a Bokeh figure() object and applies style elements meant for dotplots
+
+        Parameter: Bokeh figure() object
+
+        Return: figure() object with style applied
+    """
+
     #Applies default style to get font styles and sizes
     p = apply_default_style(p)
-
-    #Remove background color
-    #p.background_fill_color = "#FFFFFF"
-    #p.background_fill_alpha = 1
 
     #Customize grids
     p.grid.grid_line_color = "lightgrey"
     p.grid.grid_line_alpha = 0.9
     p.grid.minor_grid_line_color = "lightgrey"
-    #Weakier grids for minor ticks
-    p.grid.minor_grid_line_alpha = 0.5
+    p.grid.minor_grid_line_alpha = 0.5 #Weakier grids for minor ticks
     p.grid.minor_grid_line_dash = "dotdash"
 
     return p
 
 def apply_map_style(p):
+    """
+
+    Receives a Bokeh figure() object and applies style elements meant for the map
+
+        Parameter: Bokeh figure() object
+
+        Return: figure() object with style applied
+    """
+
+    #Applies default style to get font styles and sizes
     p = apply_default_style(p)
 
+    #Change background and gridlines to fit the map colors
     p.background_fill_color = "lightblue"
     p.background_fill_alpha = 1
-
     p.grid.grid_line_color = "white"
     p.grid.grid_line_alpha = 0.5
 
+    #Remove minor ticks
     p.xaxis[0].ticker.num_minor_ticks = 0
     p.yaxis[0].ticker.num_minor_ticks = 0
 
+    #Remove tick labels
     p.axis.major_label_text_font_size = "0px"
 
     return p
