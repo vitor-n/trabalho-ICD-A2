a151dd06f999f591895fb4f3f8f5b2b220fafb2b
Vitor <vitor.mnw@gmail.com>
2023-06-30 12:54:46 -0300

    add texts for the graph, add smooth scroll when the graph is changed

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 var card = document.getElementById('buttons-container');
 var graph = document.getElementById('graph');
+var text1 = document.getElementById('text-graph1');
+var text2 = document.getElementById('text-graph2');
+var text3 = document.getElementById('text-graph3');
+var text4 = document.getElementById('text-graph4');
+var text5 = document.getElementById('text-graph5');
+var text6 = document.getElementById('text-graph6');
+var text7 = document.getElementById('text-graph7');
+var text8 = document.getElementById('text-graph8');
+var text9 = document.getElementById('text-graph9');
 var title = document.getElementById("title");
 var text = document.getElementById("text");
 
@@ -17,61 +26,74 @@ divs.forEach(function (element) {
     });
     
     element.addEventListener("click", function () {
+        [].forEach.call(document.querySelectorAll('.text-container'), function (el) {
+            el.style.display = 'none';
+        });
+        
         graph.style.height = "750px";
 
         // Get the specific action based on the element's attributes or content
         if (element.getAttribute("id") === "graph1") {
+            window.scrollTo({top: 1, behavior: 'smooth'});
             graph.src = "graphs/larissa/graph1.html";
             graph.style.width = "1200px";
-            window.scrollTo(0, 0);
+            text1.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph2") {
+            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/larissa/graph2.html";
             graph.style.width = "1200px";
-            window.scrollTo(0, 0);
+            text2.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph3") {
+            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/larissa/graph3.html";
             graph.style.width = "1200px";
-            window.scrollTo(0, 0);
+            text3.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph4") {
+            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/pedro/graph1.html";
             graph.style.width = "1200px";
-            window.scrollTo(0, 0);
+            text4.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph5") {
+            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/pedro/graph2.html";
             graph.style.width = "1200px";
-            window.scrollTo(0, 0);
+            text5.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph6") {
+            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/pedro/graph3.html";
             graph.style.width = "1200px";
-            window.scrollTo(0, 0);
+            text6.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph7") {
+            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/vitor/graph1.html";
             graph.style.width = "750px";
-            window.scrollTo(0, 0);
+            text7.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph8") {
+            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/vitor/graph2.html";
             graph.style.width = "1200px";
-            window.scrollTo(0, 0);
+            text8.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph9") {
+            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/vitor/graph3.html";
             graph.style.width = "1200px";
-            window.scrollTo(0, 0);
+            text9.style.display = "initial";
             
         }
     });
