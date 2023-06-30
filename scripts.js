d23e2e585ef622fb81660c6c346a0bba5c0eb4f0
Vitor <vitor.mnw@gmail.com>
2023-06-30 13:01:06 -0300

    fix window not scrolling when the new text was shorter

--- scripts.js
+++ scripts.js
@@ -26,71 +26,65 @@ divs.forEach(function (element) {
     });
     
     element.addEventListener("click", function () {
+        // Hides every text container
         [].forEach.call(document.querySelectorAll('.text-container'), function (el) {
             el.style.display = 'none';
         });
         
-        graph.style.height = "750px";
+        
+        // Scroll to the top of the window
+        window.scrollTo({top: 0, behavior: 'smooth'});
 
         // Get the specific action based on the element's attributes or content
         if (element.getAttribute("id") === "graph1") {
-            window.scrollTo({top: 1, behavior: 'smooth'});
             graph.src = "graphs/larissa/graph1.html";
             graph.style.width = "1200px";
             text1.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph2") {
-            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/larissa/graph2.html";
             graph.style.width = "1200px";
             text2.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph3") {
-            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/larissa/graph3.html";
             graph.style.width = "1200px";
             text3.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph4") {
-            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/pedro/graph1.html";
             graph.style.width = "1200px";
             text4.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph5") {
-            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/pedro/graph2.html";
             graph.style.width = "1200px";
             text5.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph6") {
-            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/pedro/graph3.html";
             graph.style.width = "1200px";
             text6.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph7") {
-            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/vitor/graph1.html";
             graph.style.width = "750px";
             text7.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph8") {
-            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/vitor/graph2.html";
             graph.style.width = "1200px";
             text8.style.display = "initial";
             
 
         } else if (element.getAttribute("id") === "graph9") {
-            window.scrollTo({top: 0, behavior: 'smooth'});
             graph.src = "graphs/vitor/graph3.html";
             graph.style.width = "1200px";
             text9.style.display = "initial";
