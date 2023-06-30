17377ba4ce4afce56c5788bd768d7256a40e6e62
Vitor <vitor.mnw@gmail.com>
2023-06-30 19:04:19 -0300

    feat previous and next buttons to switch displayed graphs

--- scripts.js
+++ scripts.js
@@ -1,87 +1,147 @@
-var card = document.getElementById('buttons-container');
-var graph = document.getElementById('graph');
-var text1 = document.getElementById('text-graph1');
-var text2 = document.getElementById('text-graph2');
-var text3 = document.getElementById('text-graph3');
-var text4 = document.getElementById('text-graph4');
-var text5 = document.getElementById('text-graph5');
-var text6 = document.getElementById('text-graph6');
-var text7 = document.getElementById('text-graph7');
-var text8 = document.getElementById('text-graph8');
-var text9 = document.getElementById('text-graph9');
-var title = document.getElementById("title");
-var text = document.getElementById("text");
+var graphFrame = document.getElementById('graph-iframe');
+backButton = document.getElementById("back");
+nextButton = document.getElementById("next");
+number = document.getElementById("number");
+
+const graphs = {
+    graph0: {
+        src: "",
+        width: "0px",
+        height: "0px",
+        text: document.getElementById('text-home'),
+        graphId: 0
+    },
+    graph1: {
+        src: "graphs/larissa/graph1.html",
+        width: "1200px",
+        height: "750px",
+        text: document.getElementById('text-graph1'),
+        graphId: 1
+    },
+    graph2: {
+        src: "graphs/larissa/graph2.html",
+        width: "1200px",
+        height: "750px",
+        text: document.getElementById('text-graph2'),
+        graphId: 2
+    },
+    graph3: {
+        src: "graphs/larissa/graph3.html",
+        width: "1200px",
+        height: "750px",
+        text: document.getElementById('text-graph3'),
+        graphId: 3
+    },
+    graph4: {
+        src: "graphs/pedro/graph1.html",
+        width: "1200px",
+        height: "750px",
+        text: document.getElementById('text-graph4'),
+        graphId: 4
+    },
+    graph5: {
+        src: "graphs/pedro/graph2.html",
+        width: "1200px",
+        height: "750px",
+        text: document.getElementById('text-graph5'),
+        graphId: 5
+    },
+    graph6: {
+        src: "graphs/pedro/graph3.html",
+        width: "1200px",
+        height: "750px",
+        text: document.getElementById('text-graph6'),
+        graphId: 6
+    },
+    graph7: {
+        src: "graphs/vitor/graph1.html",
+        width: "750px",
+        height: "750px",
+        text: document.getElementById('text-graph7'),
+        graphId: 7
+    },
+    graph8: {
+        src: "graphs/vitor/graph2.html",
+        width: "1200px",
+        height: "750px",
+        text: document.getElementById('text-graph8'),
+        graphId: 8
+    },
+    graph9: {
+        src: "graphs/vitor/graph3.html",
+        width: "1200px",
+        height: "750px",
+        text: document.getElementById('text-graph9'),
+        graphId: 9
+    }
+};
 
 const divs = document.querySelectorAll('.preview-card');
+var graphNumber = 0;
 
 divs.forEach(function (element) {
-
     element.addEventListener("click", function () {
-        // Hides every text container
-        [].forEach.call(document.querySelectorAll('.text-container'), function (el) {
-            el.style.display = 'none';
-        });
-        
-        graph.style.height = "750px";
-
-        // Scroll to the top of the window
-        window.scrollTo({top: 0, behavior: 'smooth'});
 
         // Get the specific action based on the element's attributes or content
-        if (element.getAttribute("id") === "graph1") {
-            graph.src = "graphs/larissa/graph1.html";
-            graph.style.width = "1200px";
-            text1.style.display = "initial";
-            
+        var id = element.getAttribute("id");
 
-        } else if (element.getAttribute("id") === "graph2") {
-            graph.src = "graphs/larissa/graph2.html";
-            graph.style.width = "1200px";
-            text2.style.display = "initial";
-            
+        get_content(id);
 
-        } else if (element.getAttribute("id") === "graph3") {
-            graph.src = "graphs/larissa/graph3.html";
-            graph.style.width = "1200px";
-            text3.style.display = "initial";
-            
+        graphNumber = graphs[id].graphId
 
-        } else if (element.getAttribute("id") === "graph4") {
-            graph.src = "graphs/pedro/graph1.html";
-            graph.style.width = "1200px";
-            text4.style.display = "initial";
-            
-
-        } else if (element.getAttribute("id") === "graph5") {
-            graph.src = "graphs/pedro/graph2.html";
-            graph.style.width = "1200px";
-            text5.style.display = "initial";
-            
-
-        } else if (element.getAttribute("id") === "graph6") {
-            graph.src = "graphs/pedro/graph3.html";
-            graph.style.width = "1200px";
-            text6.style.display = "initial";
-            
-
-        } else if (element.getAttribute("id") === "graph7") {
-            graph.src = "graphs/vitor/graph1.html";
-            graph.style.width = "750px";
-            text7.style.display = "initial";
-            
-
-        } else if (element.getAttribute("id") === "graph8") {
-            graph.src = "graphs/vitor/graph2.html";
-            graph.style.width = "1200px";
-            text8.style.display = "initial";
-            
-
-        } else if (element.getAttribute("id") === "graph9") {
-            graph.src = "graphs/vitor/graph3.html";
-            graph.style.width = "1200px";
-            text9.style.display = "initial";
-            
-        }
     });
 });
 
+function get_content(id) {
+    // Hides every text container
+    document.querySelectorAll('.text-container').forEach(function (el) {
+        el.style.display = 'none';
+    });
+    window.scrollTo({ top: 1, behavior: 'smooth' });
+    
+    if (graphs.hasOwnProperty(id)) {
+        var selectedElement = graphs[id];
+        graphFrame.src = selectedElement.src;
+        graphFrame.style.width = selectedElement.width;
+        graphFrame.style.height = selectedElement.height;
+        selectedElement.text.style.display = "initial";
+
+        if (id != "graph0") {
+            backButton.disabled = false;
+        }
+    }
+}
+
+function ready() {
+    backButton.disabled = true;
+}
+
+function home() {
+    graphNumber = 0;
+
+    backButton.disabled = true;
+
+    get_content("graph" + graphNumber)
+}
+
+function next() {
+    graphNumber++;
+    if (graphNumber == 9) {
+        nextButton.disabled = true;
+    }
+    backButton.disabled = false;
+
+    get_content("graph" + graphNumber)
+}
+
+function back() {
+    graphNumber--;
+    if (graphNumber == 0) {
+        backButton.disabled = true;
+        graphFrame.style.height = "0";
+    }
+    nextButton.disabled = false;
+
+    get_content("graph" + graphNumber)
+
+}
